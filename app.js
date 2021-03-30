31c94d9ae21094b15ecde969cebd3b78f269b1b7
Shari Pavoni <sharidkpavoni@gmail.com>
2021-03-31 00:32:25 +0300

    Button fuctionality test

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-//
+// Selectors
+const addKmButton = document.getElementById('button1');
+const kmCounter = document.querySelector('.km-counter')
 // Event Listeners
+addKmButton.addEventListener('click',switchToAddKmScreen);
+
+
 // Functions
+function switchToAddKmScreen(event) {
+    kmCounter.classList.toggle('switched1');
+}
