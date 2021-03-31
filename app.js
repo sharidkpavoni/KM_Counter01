d647d21fbb488e0dd206f4312761e343407f2ecd
Shari Pavoni <sharidkpavoni@gmail.com>
2021-03-31 17:47:48 +0300

    No more transitions for the moment

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 // Selectors
-const addKmButton = document.getElementById('button1');
-const kmCounter = document.querySelector('.km-counter')
+
 // Event Listeners
-addKmButton.addEventListener('click',switchToAddKmScreen);
+
 
 
 // Functions
-function switchToAddKmScreen(event) {
-    kmCounter.classList.toggle('switched1');
-}
